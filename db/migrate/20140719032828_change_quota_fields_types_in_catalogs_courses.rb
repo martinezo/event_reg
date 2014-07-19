b38c4e096d74c499811e6740442ed6a8e234baa9
Ramón Martínez Olvera <martinezo@inb.unam.mx>
2014-07-18 23:32:50 -0500

    Cambios menores en vista detalle de cursos

--- db/migrate/20140719032828_change_quota_fields_types_in_catalogs_courses.rb
+++ db/migrate/20140719032828_change_quota_fields_types_in_catalogs_courses.rb
@@ -1,8 +1,8 @@
 class ChangeQuotaFieldsTypesInCatalogsCourses < ActiveRecord::Migration
   def change
-    change_table :catalogs_courses do |t|
-      t.change :catalogs_courses, :min_quota, :integer
-      t.change :catalogs_courses, :max_quota, :integer
-    end
+#    change_table :catalogs_courses do |t|
+#      t.change :catalogs_courses, :min_quota, :integer
+#      t.change :catalogs_courses, :max_quota, :integer
+#    end
   end
 end
