7ab3602226054a635a9e64930e87e4a4690f96c3
Ramón Martínez Olvera <ramonmtzol@gmail.com>
2014-07-20 19:54:05 -0500

    Change  type to min an max quota in courses

--- db/migrate/20140719032828_change_quota_fields_types_in_catalogs_courses.rb
+++ db/migrate/20140719032828_change_quota_fields_types_in_catalogs_courses.rb
@@ -1,8 +1,6 @@
 class ChangeQuotaFieldsTypesInCatalogsCourses < ActiveRecord::Migration
   def change
-#    change_table :catalogs_courses do |t|
-#      t.change :catalogs_courses, :min_quota, :integer
-#      t.change :catalogs_courses, :max_quota, :integer
-#    end
+    change_column :catalogs_courses, :min_quota, :integer
+    change_column :catalogs_courses, :max_quota, :integer
   end
 end
